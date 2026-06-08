a78103102cd2164ae0ad00b4a2a9b8a9b0a0e6d7
xerrortm <wtschengzhang7@gmail.com>
2026-06-08 18:44:41 +0400

    Update clock.js

--- scripts/clock.js
+++ scripts/clock.js
@@ -1,18 +1,28 @@
 (()=>{
 const m=["January","February","March","April","May","June","July","August","September","October","November","December"];
+const w=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+function f(t,d,e){
+return t.replace(/h|m|s|D|M|Y|Wn|W/g,x=>(
+x==="h"?String(d.getHours()).padStart(2,"0"):
+x==="m"?String(d.getMinutes()).padStart(2,"0"):
+x==="s"?String(d.getSeconds()).padStart(2,"0"):
+x==="D"?String(d.getDate()).padStart(2,"0"):
+x==="M"?e?m[d.getMonth()]:String(d.getMonth()+1).padStart(2,"0"):
+x==="Y"?d.getFullYear():
+x==="Wn"?d.getDay():
+w[d.getDay()]
+));
+}
 function u(){
 let d=new Date;
 document.querySelectorAll("[data-clock],[data-date]").forEach(e=>{
-let f=e.dataset.clock||e.dataset.date;
-e.textContent=f
-.replace(/h/g,String(d.getHours()).padStart(2,"0"))
-.replace(/m/g,String(d.getMinutes()).padStart(2,"0"))
-.replace(/s/g,String(d.getSeconds()).padStart(2,"0"))
-.replace(/D/g,String(d.getDate()).padStart(2,"0"))
-.replace(/M/g,e.dataset.date?m[d.getMonth()]:String(d.getMonth()+1).padStart(2,"0"))
-.replace(/Y/g,d.getFullYear());
+e.textContent=f(
+e.dataset.clock||e.dataset.date,
+d,
+e.dataset.date
+);
 });
 }
 u();
-setInterval(u,100);
+setInterval(u,1000);
 })();
