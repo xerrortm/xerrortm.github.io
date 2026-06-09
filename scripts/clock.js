95f2083d62e7d8c08a9c59e11cb54475b771a26a
xerrortm <wtschengzhang7@gmail.com>
2026-06-09 19:35:36 +0400

    Update clock.js

--- scripts/clock.js
+++ scripts/clock.js
@@ -3,21 +3,27 @@ const m=["January","February","March","April","May","June","July","August","Sept
 const w=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 function f(t,d,txt){
 return t.replace(/h|m|s|D|M|Y|W/g,x=>{
-if(x==="h")return String(d.getHours()).padStart(2,"0");
-if(x==="m")return String(d.getMinutes()).padStart(2,"0");
-if(x==="s")return String(d.getSeconds()).padStart(2,"0");
-if(x==="D")return String(d.getDate()).padStart(2,"0");
-if(x==="M")return txt?m[d.getMonth()]:String(d.getMonth()+1).padStart(2,"0");
-if(x==="Y")return d.getFullYear();
+if(x=="h")return String(d.getHours()).padStart(2,"0");
+if(x=="m")return String(d.getMinutes()).padStart(2,"0");
+if(x=="s")return String(d.getSeconds()).padStart(2,"0");
+if(x=="D")return String(d.getDate()).padStart(2,"0");
+if(x=="M")return txt?m[d.getMonth()]:String(d.getMonth()+1).padStart(2,"0");
+if(x=="Y")return d.getFullYear();
 return txt?w[d.getDay()]:d.getDay();
 });
 }
 function u(){
-let d=new Date;
+let n=new Date;
 document.querySelectorAll("[clock-time],[clock-date]").forEach(e=>{
-e.textContent=f(e.dataset.time||e.dataset.date,d,e.dataset.date);
+let z=e.getAttribute("clock-zone"),
+d=z==null?n:new Date(n.getTime()+(+z*60+n.getTimezoneOffset())*6e4);
+e.textContent=f(
+e.getAttribute("clock-time")||e.getAttribute("clock-date"),
+d,
+e.hasAttribute("clock-date")
+);
 });
 }
 u();
-setInterval(u,100);
+setInterval(u,500);
 })();
